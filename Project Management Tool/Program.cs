d67dbde278efbe212c08460d53baed8b1012d82a
Julien Doolub <doolubjulien@gmail.com>
2025-04-12 22:33:03 +0400

    project plan

--- Project Management Tool/Program.cs
+++ Project Management Tool/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        /*
+            Design a task/project management tool that allows users to:
+            •	Create and manage tasks and projects.
+            •	Track deadlines, priority levels, and progress of each task.
+            •	Mark tasks as completed.
+            •	Sort and filter tasks by different criteria (e.g., due date, priority).
+            •	Notify users about task status updates.
+        */
     }
 }
