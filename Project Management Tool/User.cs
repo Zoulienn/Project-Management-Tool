b4c923075f28da6ec7bf9fab1077006e1ab99bc5
Julien Doolub <doolubjulien@gmail.com>
2025-04-13 17:58:13 +0400

    User methods

--- Project Management Tool/User.cs
+++ Project Management Tool/User.cs
@@ -11,6 +11,7 @@ namespace Project_Management_Tool;
 	•	AssignedProjects (List)
     
 	•	Methods:
+    •	AddProject(Project project) - add projects to user
 	•	AssignTask(Task task, Project project) — assign a task to a specific project
 	•	GetAllTasks() — retrieve all tasks assigned to the user
 	•	GetCompletedTasks() — retrieve all completed tasks
@@ -18,7 +19,7 @@ namespace Project_Management_Tool;
 public class User
 {
     public string UserName { get; set;}
-    private string UserPassword;
+    private string UserPassword = string.Empty;
     public string Password { 
         get { return UserPassword;}
         private set { 
@@ -39,4 +40,36 @@ public class User
         this.UserProjects = new List<Project>();
     }
 
+    public void AddProject(Project project)
+    {
+        this.UserProjects.Add(project);
+    }
+
+    public void AssignTask(Task task, Project project)
+    {
+        if (!UserProjects.Contains(project))
+        {
+            throw new InvalidOperationException("User does not own this project.");
+        }
+        else
+        {
+            project.AddTask(task);
+        }
+    }
+
+    public List<Task> GetAllTasks()
+    {
+        // SelectMany as ONE project has MANY tasks and the user has MANY projects
+        return UserProjects.SelectMany(project => project.Tasks)
+                           .OrderBy(task => task.DueDate)
+                           .ToList();
+    }
+
+    public List<Task> GetCompletedTasks()
+    {
+        return UserProjects.SelectMany(project => project.Tasks)
+                           .Where(task => task.TaskStatus == Status.Completed)
+                           .OrderBy(task => task.TaskPriority)
+                           .ToList();   
+    }
 }
