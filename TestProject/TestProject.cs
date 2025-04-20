99a5142ac3f7726f2b3037ec671cd87ec5405a7e
Julien Doolub <doolubjulien@gmail.com>
2025-04-20 16:56:59 +0400

    cleaner TestProject

--- TestProject/TestProject.cs
+++ TestProject/TestProject.cs
@@ -11,44 +11,48 @@ namespace TestProject
     [TestClass]
     public class TestProject
     {
+        private Project _project = null!;
+        private DateTime _today;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _today = DateTime.Today;
+            _project = new Project("Test Project", _today, _today.AddDays(30));
+        }
+
         [TestMethod]
         public void Constructor_ShouldInitializeProjectCorrectly()
         {
-            var project = new Project("AI Dev", DateTime.Today, DateTime.Today.AddDays(30));
-
-            Assert.AreEqual("AI Dev", project.ProjectName);
-            Assert.AreEqual(DateTime.Today, project.StartDate);
-            Assert.AreEqual(DateTime.Today.AddDays(30), project.EndDate);
-            Assert.AreEqual(0, project.Tasks.Count);
+            Assert.AreEqual("Test Project", _project.ProjectName);
+            Assert.AreEqual(_today, _project.StartDate);
+            Assert.AreEqual(_today.AddDays(30), _project.EndDate);
+            Assert.AreEqual(0, _project.Tasks.Count);
         }
 
         [TestMethod]
         public void AddTask_ShouldAddTaskAndLinkToProject()
         {
-            var project = new Project("Backend Refactor", DateTime.Today, DateTime.Today.AddDays(15));
-            var task = new UserTask("Refactor Models", "Refactor legacy models to new structure", DateTime.Today.AddDays(2), Priority.Medium);
+            var task = new UserTask("Refactor Models", "Refactor legacy models", _today.AddDays(2), Priority.Medium);
+            _project.AddTask(task);
 
-            project.AddTask(task);
-
-            Assert.AreEqual(1, project.Tasks.Count);
-            Assert.AreEqual(project, task.Project);
+            Assert.AreEqual(1, _project.Tasks.Count);
+            Assert.AreEqual(_project, task.Project);
         }
 
         [TestMethod]
         public void GetPendingTasks_ShouldReturnOnlyNonCompletedTasks()
         {
-            var project = new Project("Mobile App", DateTime.Today, DateTime.Today.AddDays(20));
-
-            var task1 = new UserTask("Design UI", "Create wireframes", DateTime.Today.AddDays(3), Priority.High);
-            var task2 = new UserTask("Setup Firebase", "Integrate DB", DateTime.Today.AddDays(5), Priority.Medium);
-            var task3 = new UserTask("Push Notification", "Enable FCM", DateTime.Today.AddDays(4), Priority.Low);
+            var task1 = new UserTask("Design UI", "Create wireframes", _today.AddDays(3), Priority.High);
+            var task2 = new UserTask("Setup Firebase", "Integrate DB", _today.AddDays(5), Priority.Medium);
+            var task3 = new UserTask("Push Notification", "Enable FCM", _today.AddDays(4), Priority.Low);
             task2.CompleteTask();
 
-            project.AddTask(task1);
-            project.AddTask(task2);
-            project.AddTask(task3);
+            _project.AddTask(task1);
+            _project.AddTask(task2);
+            _project.AddTask(task3);
 
-            var pendingTasks = project.GetPendingTasks();
+            var pendingTasks = _project.GetPendingTasks();
 
             Assert.AreEqual(2, pendingTasks.Count);
             Assert.IsFalse(pendingTasks.Any(t => t.TaskStatus == Status.Completed));
@@ -57,21 +61,19 @@ namespace TestProject
         [TestMethod]
         public void GetOverdueTasks_ShouldReturnOnlyOverdueAndNotCompleted()
         {
-            var project = new Project("WebApp", DateTime.Today.AddDays(-10), DateTime.Today.AddDays(10));
+            var task1 = new UserTask("Bug Fixes", "Fix issues", _today.AddDays(-5), Priority.High);
+            var task2 = new UserTask("Deployment", "Push to Azure", _today.AddDays(-2), Priority.Medium);
+            var task3 = new UserTask("Write Docs", "Docs", _today.AddDays(3), Priority.Low);
+            var task4 = new UserTask("QA Testing", "Regression test", _today.AddDays(-1), Priority.High);
 
-            var task1 = new UserTask("Bug Fixes", "Fix reported issues", DateTime.Today.AddDays(-5), Priority.High);
-            var task2 = new UserTask("Deployment", "Push to Azure", DateTime.Today.AddDays(-2), Priority.Medium);
-            var task3 = new UserTask("Write Docs", "Technical docs", DateTime.Today.AddDays(3), Priority.Low);
-            var task4 = new UserTask("QA Testing", "Regression test", DateTime.Today.AddDays(-1), Priority.High);
-            
             task2.CompleteTask();
 
-            project.AddTask(task1);
-            project.AddTask(task2);
-            project.AddTask(task3);
-            project.AddTask(task4);
+            _project.AddTask(task1);
+            _project.AddTask(task2);
+            _project.AddTask(task3);
+            _project.AddTask(task4);
 
-            var overdueTasks = project.GetOverdueTasks();
+            var overdueTasks = _project.GetOverdueTasks();
 
             Assert.AreEqual(2, overdueTasks.Count);
             Assert.IsTrue(overdueTasks.All(t => t.DueDate < DateTime.Now && t.TaskStatus != Status.Completed));
@@ -80,17 +82,15 @@ namespace TestProject
         [TestMethod]
         public void GetTasksByPriority_ShouldReturnTasksFilteredByPriority()
         {
-            var project = new Project("CRM Tool", DateTime.Today, DateTime.Today.AddDays(25));
+            var task1 = new UserTask("Set Up Auth", "Login flow", _today.AddDays(2), Priority.High);
+            var task2 = new UserTask("Build Dashboard", "Admin UI", _today.AddDays(4), Priority.Medium);
+            var task3 = new UserTask("Optimize Queries", "Speed up endpoints", _today.AddDays(1), Priority.High);
 
-            var task1 = new UserTask("Set Up Auth", "Implement login", DateTime.Today.AddDays(2), Priority.High);
-            var task2 = new UserTask("Build Dashboard", "Admin UI", DateTime.Today.AddDays(4), Priority.Medium);
-            var task3 = new UserTask("Optimize Queries", "Speed up endpoints", DateTime.Today.AddDays(1), Priority.High);
+            _project.AddTask(task1);
+            _project.AddTask(task2);
+            _project.AddTask(task3);
 
-            project.AddTask(task1);
-            project.AddTask(task2);
-            project.AddTask(task3);
-
-            var highPriorityTasks = project.GetTasksByPriority(Priority.High);
+            var highPriorityTasks = _project.GetTasksByPriority(Priority.High);
 
             Assert.AreEqual(2, highPriorityTasks.Count);
             Assert.IsTrue(highPriorityTasks.All(t => t.TaskPriority == Priority.High));
@@ -101,8 +101,7 @@ namespace TestProject
         [ExpectedException(typeof(ArgumentNullException))]
         public void AddTask_NullTask_ShouldThrowArgumentNullException()
         {
-            var project = new Project("Error Handling", DateTime.Today, DateTime.Today.AddDays(5));
-            project.AddTask(null!);
+            _project.AddTask(null!);
         }
     }
 }
